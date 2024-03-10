24ecf9595472c614db0cce28dbfad10355fe7b95
나경호 <gangho324@gmail.com>
2024-03-11 01:29:28 +0900

    Initial commit

---
+++ src/main/java/com/discorddev/dorbot/DorbotApplication.java
@@ -0,0 +1,13 @@
+package com.discorddev.dorbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DorbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DorbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discorddev/dorbot/DorbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discorddev.dorbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DorbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
