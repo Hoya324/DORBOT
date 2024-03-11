d3e61d160e6a6f33a5b490de56cf9b701292f526
나경호 <gangho324@gmail.com>
2024-03-11 13:46:09 +0900

    feat: discord command

---
+++ src/main/java/com/discorddev/dorbot/discord/command/DiscordCommand.java
@@ -0,0 +1,12 @@
+package com.discorddev.dorbot.discord.command;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DiscordCommand {
+
+
+}
